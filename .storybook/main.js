8ce74afc6ef4106c87c5d46e0bd8612f22186498
Vincent Delaleu <vincent@argil-data.agency>
2024-07-20 18:59:28 +0200

    add: storybook init

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+	stories: [
+		"../stories/**/*.mdx",
+		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+	],
+	core: {
+		disableTelemetry: true, // 👈 Disables telemetry
+	},
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+};
+export default config;
